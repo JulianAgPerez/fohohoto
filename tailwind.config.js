0dc44b58c1195e18a827229d496ca567a582a847
JulianAgPerez <juli.agustin.perez@gmail.com>
2026-08-17 19:10:39 -0300

    feat(theme): add berry color token to unify red and rose palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,21 @@
 export default {
   content: [ "./src/**/*.{js,jsx,ts,tsx}", ],
   theme: {
-  extend: {
-    fontFamily: {
-      sans: ['Quicksand', 'ui-sans-serif', 'system-ui', 'sans-serif'],
-      christmas: ['"Mountains of Christmas"', 'cursive'],
+    extend: {
+      fontFamily: {
+        sans: ['Quicksand', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        christmas: ['"Mountains of Christmas"', 'cursive'],
+      },
+      colors: {
+        berry: {
+          300: '#fca5a5', // red-300
+          400: '#f87171', // red-400
+          500: '#ef4444', // red-500
+          600: '#dc2626', // red-600
+          700: '#be123c', // rose-700
+        },
+      },
     },
-  },
   },
   plugins: [],
 }
-
